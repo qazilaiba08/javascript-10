d65825fa06cbaf9a9bc938addb3ca92d70cd0ab7
Laiba Qazi <164245574+qazilaiba08@users.noreply.github.com>
2024-05-13 15:49:28 +0500

    chap 10

---
+++ script.js
@@ -0,0 +1,56 @@
+
+
+// a++ ;//post increment
+// ++a ; //pre increment
+
+// let a = 10;
+// // console.log(++a);
+// console.log(a++);
+// console.log(a++);
+// console.log(a++);
+// console.log(a++);
+
+// let number1 = +prompt('Enter Your First Number',20);
+// let number2 = +prompt('Enter Your Second Number');
+// console.log(number1 + number2);
+
+//opreters +=;
+//a +=2;
+// a = a + 2;
+// a = a * 2;
+// a = a % 2 ;
+// a = a / 2;
+// 2 ** 2;
+
+//comparsion opreteros
+
+// let f = 18;
+// let z = 18;
+// console.log(f!==z);
+
+// let mode = "yellow";
+// let color = "black";
+// if(mode == "dark"){
+//     console.log(color); 
+// }
+//  else{
+//     console.log("white");
+//  }
+let hello ;
+console.log(hello);
+
+// let age = +prompt("Enter Your Age");
+// if(age >= 18){
+//     console.log('You are eligable for voting');
+// }
+// else{
+//     console.log("You are not eligable for voting");
+// }
+
+let number = +prompt('Enter Your Number');
+if (number % 2 == 0) {
+    console.log( number,"is an even number");
+}
+else{
+    console.log(number, "is an odd number");
+}
